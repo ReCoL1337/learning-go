1f9dfb677ff83799a29bf43c21ea0e196fdd4ffe
ReCoL1337 <nataniel2003@gmail.com>
2024-05-25 15:21:00 +0200

    Back to learning

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module hello_world
 
-go 1.22.2
+go 1.22.3
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const value = 10
+
+func main() {
+    var i int = value
+    var j float64 = value
+    fmt.Println(i, j)
+}
